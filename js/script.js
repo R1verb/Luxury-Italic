65d2c859af934c1bfba0e6a0fbc0a19ebf2897a1
R1verb <128699019+R1verb@users.noreply.github.com>
2024-03-07 21:40:22 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,24 +5,30 @@ const mobileMenuLinks = document.querySelectorAll('.nav_link');
 const headerBtn = document.querySelectorAll('.header_btn');
 
 function closeMenu() {
-    if (window.innerWidth > 968) {
-        mobileMenu.classList.remove('burger_menu-active');
-        navList.classList.remove('nav_list-active');
-        headerBtn.forEach(btn => {
-            btn.classList.remove('header_btn-active');
-        });
-        bodyScroll.classList.remove('scroll');
-    }
+    mobileMenu.classList.remove('burger_menu-active');
+    navList.classList.remove('nav_list-active');
+    headerBtn.forEach(btn => {
+        btn.classList.remove('header_btn-active');
+    });
+    bodyScroll.classList.remove('scroll');
 }
 
 function toggleMenu() {
     mobileMenu.classList.toggle('burger_menu-active');
     navList.classList.toggle('nav_list-active');
     headerBtn.forEach(btn => {
-        btn.classList.toggle('header_btn-active');});
+        btn.classList.toggle('header_btn-active');
+    });
     bodyScroll.classList.toggle('scroll');
     if (mobileMenu.classList.contains('burger_menu-active')) {
         window.scrollTo(0, 0);
+    }
+}
+
+function closeMenuOnClickOutside(event) {
+    const isClickInsideMenu = mobileMenu.contains(event.target);
+    if (!isClickInsideMenu) {
+        closeMenu();
     }
 }
 
@@ -37,3 +43,5 @@ window.addEventListener('load', () => {
     closeMenu();
     window.scrollTo(0, 0);
 });
+
+document.addEventListener('click', closeMenuOnClickOutside);
